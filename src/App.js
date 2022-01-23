c040216215d2aa52c64c7ade4516cd805a6c5ea1
Shivam Misra <shivam.misra03@gmail.com>
2022-01-23 18:44:22 +0530

    React : tutorial 7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 let name = 'Shivam';
 function App() {
@@ -8,6 +9,10 @@ function App() {
     <>
       <Navbar title = "TextUtils2" aboutText = "About Text Utils"/>
       {/* <Navbar/> */}
+      <div className="container my-3">
+        <TextForm heading = "Enter the text to analyze"/>
+      </div>
+      
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+    const [text, setText] = useState('Enter text here');
+
+    const handleUpClick = () => {
+        console.log("button was clicked")
+        setText(text.toUpperCase())
+    }
+
+    const handleOnChange = (event) => {
+        console.log("On change");
+        setText(event.target.value);
+    }
+    return (
+        <div>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                {/* <label for="myBox" className="form-label">Example textarea</label> */}
+                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+        </div>
+    )
+}
