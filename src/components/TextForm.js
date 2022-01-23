10a36fa1c18b7060d77b4c376d3c43f4f03976c3
Shivam Misra <shivam.misra03@gmail.com>
2022-01-23 19:40:49 +0530

    React :  tutorial 8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
 
     const handleUpClick = () => {
         console.log("button was clicked")
@@ -13,14 +13,27 @@ export default function TextForm(props) {
         console.log("On change");
         setText(event.target.value);
     }
+
+    const handleDownClick = () => {
+        setText(text.toLowerCase())
+    }
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 {/* <label for="myBox" className="form-label">Example textarea</label> */}
                 <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to Lowercase</button>
         </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(' ').length} words and {text.length} characters</p>
+            <p>Your text can be read in  {0.008 * text.split(' ').length}</p>
+            <p>Preview : {text}</p>
+        </div>
+        </>
     )
 }
