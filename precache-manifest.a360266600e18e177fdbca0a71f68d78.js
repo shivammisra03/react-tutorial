3615d6158fbd8ae85fab0eec54bc1bb85b16c1bd
Shivam Misra <shivam.misra03@gmail.com>
2022-01-26 09:41:29 +0530

    Updates

---
+++ precache-manifest.a360266600e18e177fdbca0a71f68d78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9adb6923ca051ca99c65b7f639c3884",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "4771be35ce9f1dd81b4f",
+    "url": "/react-tutorial/static/css/main.1c8da05c.chunk.css"
+  },
+  {
+    "revision": "44770abcd4a12a3e0075",
+    "url": "/react-tutorial/static/js/2.74e666b0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-tutorial/static/js/2.74e666b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2650d24361ab1a6e6f5",
+    "url": "/react-tutorial/static/js/3.5753cfae.chunk.js"
+  },
+  {
+    "revision": "4771be35ce9f1dd81b4f",
+    "url": "/react-tutorial/static/js/main.0a38314d.chunk.js"
+  },
+  {
+    "revision": "e94985479b65d6322bc5",
+    "url": "/react-tutorial/static/js/runtime-main.f252d24e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-tutorial/static/media/logo.06e73328.svg"
+  }
+]);
